0780105e3aa5d653bba18dc2e9afcb9afc8f2fc3
Deidra22 <Deidra22@users.noreply.github.com>
2019-07-06 12:58:10 -0700

    First commit already worked on

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Create a loop that goes from 0-100
+// When the number is divisible by 3 I want to console.log('fizz');
+// When the number is divisible by 5 I want to console.log('buzz');
+// When the number is divisible by 3 and 5 I want to console.log('fizzbuzz');
+// for(let i = 0; i <= 100; i++) {
+
+//     if (i % 3 === 0 && i % 5 === 0){
+//         console.log('fizzbuzz');
+//     }
+//     else if (i % 5 === 0){
+//         console.log('buzz');
+//     }
+//     else if (i % 3 === 0){
+//         console.log('fizz');
+//     }
+//     else console.log(i);
+// }
